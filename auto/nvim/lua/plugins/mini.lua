c5001a481ad39b5c62667862dc42ffe5b679bd64
dvdv <dvdvnine@gmail.com>
2026-07-13 22:40:39 +0800

    feat(nvim): highlight section markers

--- auto/nvim/lua/plugins/mini.lua
+++ auto/nvim/lua/plugins/mini.lua
@@ -517,6 +517,8 @@ later(function()
   local hipatterns = require("mini.hipatterns")
   local hi_words = MiniExtra.gen_highlighter.words
 
+  vim.api.nvim_set_hl(0, "MiniHipatternsSection", { link = "MiniHipatternsNote" })
+
   hipatterns.setup({
     highlighters = {
       -- Highlight a fixed set of common words. Will be highlighted in any place,
@@ -525,6 +527,8 @@ later(function()
       hack = hi_words({ "HACK", "Hack", "hack" }, "MiniHipatternsHack"),
       todo = hi_words({ "TODO", "Todo", "todo" }, "MiniHipatternsTodo"),
       note = hi_words({ "NOTE", "Note", "note" }, "MiniHipatternsNote"),
+
+      section = hi_words({ "SECTION", "Section", "section" }, "MiniHipatternsSection"),
 
       -- Highlight hex color string (#aabbcc) with that color as a background
       hex_color = hipatterns.gen_highlighter.hex_color(),
